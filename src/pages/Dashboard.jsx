26778e6d5f6d1cf26fdc9a53deda9c8b6700f248
vivekdhumal9326 <vivekdhumal9326@gmail.com>
2026-02-15 22:03:00 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -25,7 +25,13 @@ export default function Dashboard() {
     { id: 10, name: '15000 Instagram Views', price: '₹ 2,650', count: 15000},
     { id: 11, name: '20000 Instagram Views', price: '₹ 3,550', count: 1000}, 
     { id: 12, name: '50000 Instagram Views', price: '₹ 5,900', count: 50000},
-    { id: 13, name: '100000 Instagram Views', price: '₹ 12,000', count: 100000}
+    { id: 13, name: '100000 Instagram Views', price: '₹ 12,000', count: 100000},
+    { id: 14, name: '1000 Instagram Likes', price: '₹ 400', count: 1000},
+    { id: 15, name: '5000 Instagram Likes', price: '₹ 760', count: 5000},
+    { id: 16, name: '10000 Instagram Likes', price: '₹ 1500', count: 10000},
+    { id: 17, name: '20000 Instagram Likes', price: '₹ 2730', count: 20000},
+    { id: 18, name: '50000 Instagram Likes', price: '₹ 5000', count: 50000},
+    { id: 15, name: '100000 Instagram Likes', price: '₹ 10000', count: 100000}  
     
     
   ];
